77c98f8fe67de19a138d6d8b09b841ba4ed95ba6
Andrea Hayes <ahaye74@wgu.edu>
2024-02-27 11:01:40 -0600

    B2.  Include the following details for each vacation: •  title (e.g., Bermuda Trip, Spring Break, London Trip) •  hotel or other place where you will be staying •  start date •  end date

--- app/src/main/java/com/example/d308_android/UI/VacationList.java
+++ app/src/main/java/com/example/d308_android/UI/VacationList.java
@@ -10,9 +10,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.d308_android.R;
+import com.example.d308_android.database.Repository;
+import com.example.d308_android.entities.Excursion;
+import com.example.d308_android.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationList extends AppCompatActivity {
+private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,16 @@ public class VacationList extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()==R.id.sample) {
-            Toast.makeText(VacationList.this, "put in sample data",Toast.LENGTH_LONG).show();
+            repository=new Repository(getApplication());
+            //Toast.makeText(VacationList.this, "put in sample data",Toast.LENGTH_LONG).show();
+            Vacation vacation=new Vacation(0, "Thailand", 1500, "Hilton Hotel", "02/01/24", "02/20/24");
+            repository.insert(vacation);
+            vacation=new Vacation(0, "Japan", 1500, "Miaki Hotel", "02/02/24", "02/19/24");
+            repository.insert(vacation);
+            Excursion excursion=new Excursion(0, "Garden Tour", 300, 1);
+            repository.insert(excursion);
+            excursion=new Excursion(0, "Cheese Tour", 400, 1);
+            repository.insert(excursion);
             return true;
         }
         if(item.getItemId()==android.R.id.home) {
---
+++ app/src/main/java/com/example/d308_android/entities/Vacation.java
@@ -0,0 +1,72 @@
+package com.example.d308_android.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "vacations")
+public class Vacation {
+    @PrimaryKey(autoGenerate = true)
+    private int vacationID;
+    private String vacationName;
+    private double price;
+    private String hotelName;
+    public String startDate;
+    public String endDate;
+
+    public Vacation(int vacationID, String vacationName, double price, String hotelName, String startDate, String endDate) {
+        this.vacationID = vacationID;
+        this.vacationName = vacationName;
+        this.price = price;
+        this.hotelName = hotelName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
+
+    public String getVacationName() {
+        return vacationName;
+    }
+
+    public void setVacationName(String vacationName) {
+        this.vacationName = vacationName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
